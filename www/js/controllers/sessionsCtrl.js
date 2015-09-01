8200e56697e0f3d3b550cfcbeb1d1cd7994ee35a
Jerome67000 <jerome.gully0@gmail.com>
2015-09-02 00:00:15 +0200

    Implémentation popup d'ajout d'une nouvelle session

--- www/js/controllers/sessionsCtrl.js
+++ www/js/controllers/sessionsCtrl.js
@@ -1,4 +1,26 @@
-app.controller('sessionsCtrl', function($scope) {
+app.controller('sessionsCtrl', function($scope, $ionicPopup) {
+
+  $scope.showNewSessionPopup = function() {
+    $scope.session = {};
+
+  // An elaborate, custom popup
+  var myPopup = $ionicPopup.show({
+    template: '<input type="text" placeholder="Séance #23" ng-model="session.title"><input type="text" placeholder="Salle d escalade" ng-model="session.gym"><input type="text" placeholder="note" ng-model="session.note">',
+    title: 'Nouvelle séance',
+    scope: $scope,
+    buttons: [
+      { text: 'Annuler' },
+      {
+        text: '<b>Ok</b>',
+        type: 'button-positive',
+      },
+    ]
+  });
+  myPopup.then(function(res) {
+    console.log('Tapped!', res);
+    $state.go("tab.stats");
+  });
+  };
 
   $scope.sessions = [
         {
@@ -21,7 +43,8 @@ app.controller('sessionsCtrl', function($scope) {
             "title" : "6b+ verticale"
           } ],
           "title" : "session 1"
-        }, {
+        },
+        {
           "created_at" : "2015-08-28",
           "id" : 0,
           "note" : "Très bonne séance",
@@ -41,7 +64,134 @@ app.controller('sessionsCtrl', function($scope) {
             "validate" : true
           } ],
           "title" : "session 2"
-        }, {
+        },
+        {
+          "created_at" : "2015-08-28",
+          "id" : 0,
+          "note" : "Très bonne séance",
+          "routes" : [ {
+            "climb_style" : "Moulinette",
+            "flash" : true,
+            "grade" : {
+              "is_plus" : true,
+              "letter" : "b",
+              "num" : 6
+            },
+            "id" : 0,
+            "note" : "Belle voie en dièdre",
+            "picture" : "http://fefssef.com/pict.png",
+            "route_type" : "Verticale",
+            "title" : "6b+ verticale",
+            "validate" : true
+          } ],
+          "title" : "session 2"
+        },
+        {
+          "created_at" : "2015-08-28",
+          "id" : 0,
+          "note" : "Très bonne séance",
+          "routes" : [ {
+            "climb_style" : "Moulinette",
+            "flash" : true,
+            "grade" : {
+              "is_plus" : true,
+              "letter" : "b",
+              "num" : 6
+            },
+            "id" : 0,
+            "note" : "Belle voie en dièdre",
+            "picture" : "http://fefssef.com/pict.png",
+            "route_type" : "Verticale",
+            "title" : "6b+ verticale",
+            "validate" : true
+          } ],
+          "title" : "session 2"
+        },
+        {
+          "created_at" : "2015-08-28",
+          "id" : 0,
+          "note" : "Très bonne séance",
+          "routes" : [ {
+            "climb_style" : "Moulinette",
+            "flash" : true,
+            "grade" : {
+              "is_plus" : true,
+              "letter" : "b",
+              "num" : 6
+            },
+            "id" : 0,
+            "note" : "Belle voie en dièdre",
+            "picture" : "http://fefssef.com/pict.png",
+            "route_type" : "Verticale",
+            "title" : "6b+ verticale",
+            "validate" : true
+          } ],
+          "title" : "session 2"
+        },
+        {
+          "created_at" : "2015-08-28",
+          "id" : 0,
+          "note" : "Très bonne séance",
+          "routes" : [ {
+            "climb_style" : "Moulinette",
+            "flash" : true,
+            "grade" : {
+              "is_plus" : true,
+              "letter" : "b",
+              "num" : 6
+            },
+            "id" : 0,
+            "note" : "Belle voie en dièdre",
+            "picture" : "http://fefssef.com/pict.png",
+            "route_type" : "Verticale",
+            "title" : "6b+ verticale",
+            "validate" : true
+          } ],
+          "title" : "session 2"
+        },
+        {
+          "created_at" : "2015-08-28",
+          "id" : 0,
+          "note" : "Très bonne séance",
+          "routes" : [ {
+            "climb_style" : "Moulinette",
+            "flash" : true,
+            "grade" : {
+              "is_plus" : true,
+              "letter" : "b",
+              "num" : 6
+            },
+            "id" : 0,
+            "note" : "Belle voie en dièdre",
+            "picture" : "http://fefssef.com/pict.png",
+            "route_type" : "Verticale",
+            "title" : "6b+ verticale",
+            "validate" : true
+          } ],
+          "title" : "session 2"
+        },
+        {
+          "created_at" : "2015-08-28",
+          "id" : 0,
+          "note" : "Très bonne séance",
+          "routes" : [ {
+            "climb_style" : "Moulinette",
+            "flash" : true,
+            "grade" : {
+              "is_plus" : true,
+              "letter" : "b",
+              "num" : 6
+            },
+            "id" : 0,
+            "note" : "Belle voie en dièdre",
+            "picture" : "http://fefssef.com/pict.png",
+            "route_type" : "Verticale",
+            "title" : "6b+ verticale",
+            "validate" : true
+          } ],
+          "title" : "session 2"
+        },
+         {
           "created_at" : "2015-08-28",
           "id" : 0,
           "note" : "Très bonne séance",
