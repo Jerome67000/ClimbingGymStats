e3ac7dd61f25308ade7ffd021bdbf2bdf0cdce68
Jerome67000 <jerome.gully0@gmail.com>
2015-09-05 14:01:55 +0200

    Ajout récupération des events d'une gym depuis la DB

--- www/js/controllers/eventsCtrl.js
+++ www/js/controllers/eventsCtrl.js
@@ -1,20 +1,31 @@
-app.controller('eventsCtrl', function($scope, $state) {
+app.controller('eventsCtrl', function($scope, $state, $firebaseArray) {
 
-  $scope.events = [
-    {
-      title : "Apéro roc",
-      description : "Un petit apéro comme on les aime. Ramener de quoi boire et grignotter",
-      date : "Jeudi 17 septembre 21h"
-    },
-    {
-      title : "Apéro roc",
-      description : "Un petit apéro comme on les aime. Ramener de quoi boire et grignotter",
-      date : "Jeudi 17 septembre 21h"
-    },
-    {
-      title : "Apéro roc",
-      description : "Un petit apéro comme on les aime. Ramener de quoi boire et grignotter",
-      date : "Jeudi 17 septembre 21h"
-    }
-  ];
+  var firebase = new Firebase(window.app_url);
+
+  firebase.child("gyms/0/events").on("value", function(snapshot) {
+    $scope.events = snapshot.val();  // Alerts "San Francisco"
+  });
+
+  //
+  //
+  // $scope.events = [
+  //   {
+  //     title : "Apéro roc",
+  //     description : "Un petit apéro comme on les aime. Ramener de quoi boire et grignotter",
+  //     date : "Jeudi 17 septembre 21h"
+  //   },
+  //   {
+  //     title : "Apéro roc",
+  //     description : "Un petit apéro comme on les aime. Ramener de quoi boire et grignotter",
+  //     date : "Jeudi 17 septembre 21h"
+  //   },
+  //   {
+  //     title : "Apéro roc",
+  //     description : "Un petit apéro comme on les aime. Ramener de quoi boire et grignotter",
+  //     date : "Jeudi 17 septembre 21h"
+  //   }
+  // ];
+  //
+  // firebase.child("gyms/0/events").set($scope.events);
+
 });
