1e1c1173dd31c667b87b0661baab68d69461c085
Jerome67000 <jerome.gully0@gmail.com>
2015-09-11 18:34:21 +0200

    close #4, Ajout bouton paramètres dans la nav-bar + popup

--- www/js/controllers/tabsCtrl.js
+++ www/js/controllers/tabsCtrl.js
@@ -1,4 +1,20 @@
-app.controller('tabsCtrl', function($scope) {
+app.controller('tabsCtrl', function($scope, $ionicPopup) {
   $scope.user_id = window.userUniqueId;
   $scope.gym_id = window.gymUniqueId;
+
+  $scope.openConfigPopup = function() {
+   var confirmPopup = $ionicPopup.confirm({
+     title: 'Déconnexion',
+     template: 'Etes-vous sûr de vouloir vous déconnecter ?'
+   });
+   confirmPopup.then(function(res) {
+     if(res) {
+       logout();
+     }
+   });
+  };
+
+  function logout() {
+
+  }
 });
