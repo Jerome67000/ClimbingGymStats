0ad4f2650153faf0031c6d7638d3241f7330b48b
Jerome67000 <jerome.gully0@gmail.com>
2015-10-17 13:26:20 +0200

    remove uncommented code

--- www/js/controllers/eventsCtrl.js
+++ www/js/controllers/eventsCtrl.js
@@ -15,19 +15,3 @@ app.controller('eventsCtrl', function($scope, EventsFactory) {
       });
   };
 });
-
-
-angular.module('testApp', ['ionic'])
-.controller('MyController', function($scope, $http) {
-  $scope.items = [1,2,3];
-  $scope.doRefresh = function() {
-    $http.get('/new-items')
-     .success(function(newItems) {
-       $scope.items = newItems;
-     })
-     .finally(function() {
-       // Stop the ion-refresher from spinning
-       $scope.$broadcast('scroll.refreshComplete');
-     });
-  };
-});
