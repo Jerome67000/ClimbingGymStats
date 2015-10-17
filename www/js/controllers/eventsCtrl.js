0bd28ef1377f9ed9715daf61f0774565f87babcb
Jerome67000 <jerome.gully0@gmail.com>
2015-10-17 13:19:30 +0200

    Ajout du pull-to-refresh sur la liste des évenements

--- www/js/controllers/eventsCtrl.js
+++ www/js/controllers/eventsCtrl.js
@@ -2,4 +2,32 @@ app.controller('eventsCtrl', function($scope, EventsFactory) {
 
   $scope.events = EventsFactory.all;
 
+  $scope.doRefresh = function () {
+    EventsFactory.all.$loaded().then(
+      function(x) {
+        $scope.events = EventsFactory.all;
+      }).catch(
+      function(error) {
+        console.log("Error refresh :", error);
+      }).finally(function() {
+        // Stop the ion-refresher from spinning
+        $scope.$broadcast('scroll.refreshComplete');
+      });
+  };
+});
+
+
+angular.module('testApp', ['ionic'])
+.controller('MyController', function($scope, $http) {
+  $scope.items = [1,2,3];
+  $scope.doRefresh = function() {
+    $http.get('/new-items')
+     .success(function(newItems) {
+       $scope.items = newItems;
+     })
+     .finally(function() {
+       // Stop the ion-refresher from spinning
+       $scope.$broadcast('scroll.refreshComplete');
+     });
+  };
 });
